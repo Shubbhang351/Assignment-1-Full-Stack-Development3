4e03ce1d7ab4e54bf321c78434bd82b729181a78
Shubbhang <shubhang.verma_cs18@gla.ac.in>
2020-11-04 16:16:21 +0530

    initial commit

---
+++ Live-clock/main.js
@@ -0,0 +1,32 @@
+//selector
+
+const time = document.getElementById('time');
+const greeting = document.getElementById('greeting');
+const name = document.getElementById('name');
+const day = document.getElementById('day');
+
+document.body.style.backgroundImage = "url('./1.jpg')";
+
+
+
+function showtime() {
+    let today = new Date();
+    let hour = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+    let dateT = today.toDateString();
+
+    const amPm = hour >= 12 ? 'PM' : 'AM';
+
+    hour = hour % 12 || 12;
+
+    time.innerHTML = `${addzero(hour)}<span>:</span>${addzero(minutes)}<span>:</span>${addzero(seconds)} ${amPm}`;
+    day.innerHTML = `<span>${dateT}</span>`;
+
+    setTimeout(showtime, 1000);
+}
+
+function addzero(n) {
+    return ( parseInt(n,10) < 10 ? "0" : "") + n;
+}
+showtime();
